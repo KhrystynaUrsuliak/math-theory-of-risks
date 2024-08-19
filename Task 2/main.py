cf194cba2b461ad7048e92f7ddb6e3261cabe4dc
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-08-19 12:25:28 +0300

    Created main.py

---
+++ Task 2/main.py
@@ -0,0 +1,11 @@
+a_profit_1 = 40000
+a_profit_2 = 10000
+b_profit_1 = 30000
+b_profit_2 = 5000
+
+p_a_1 = p_a_2 = p_b_1 = p_b_2 = 0.5
+
+print(f"Share Package A:\nOptimistic:\n\tProbable Profit: ${a_profit_1}\n\tProbability: {p_a_1}\nPessimistic:\n\tProbable Profit: ${a_profit_2}\n\tProbability: {p_a_2}\n")
+
+print(f"Share Package B:\nOptimistic:\n\tProbable Profit: ${b_profit_1}\n\tProbability: {p_b_1}\nPessimistic:\n\tProbable Profit: ${b_profit_2}\n\tProbability: {p_b_2}\n")
+
